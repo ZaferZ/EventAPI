1a4d839821df15bab61a92bacc809a7bd13aa269
ZaferZ <zafer.bayraktarov@gmail.com>
2025-08-05 12:43:51 +0300

    Update packages and implement JWT authentication
    - Upgraded Entity Framework Core packages to version 9.0.8.
    - Added JWT Bearer authentication and token handling packages.
    - Modified `Program.cs` to configure JWT authentication services.
    - Updated `appsettings.json` to include JWT settings for token, issuer, and audience.

--- EventAPI/Program.cs
+++ EventAPI/Program.cs
@@ -2,8 +2,11 @@ using EventAPI.Data;
 using EventAPI.Repositories;
 using EventAPI.Services;
 using Microsoft.EntityFrameworkCore;
-using EventAPI.Models;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+
+
 
 
 
@@ -23,6 +26,32 @@ builder.Services.AddDbContext<EventDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IEventService, EventService>();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
+
+// Register the JWT service
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+var secretKey = appSettingsSection.GetValue<string>("Token");
+var issuer = appSettingsSection.GetValue<string>("Issuer");
+var audience = appSettingsSection.GetValue<string>("Audience");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey)),
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = audience,
+        ValidateLifetime = true,
+    };
+});
+
+builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
